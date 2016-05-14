3a1bcc60b1e01b734722f0ec70b82240e5849b08
Emily Horsman <me@emilyhorsman.com>
2016-05-14 12:42:42 -0400

    Fuck it, let's give webpack 2.1.0-beta.7 a try!

--- deck/react/webpack.config.js
+++ deck/react/webpack.config.js
@@ -89,7 +89,9 @@ if (TARGET === 'build') {
         plugins: [
             new webpack.DefinePlugin({
                 'process.env': { 'NODE_ENV': JSON.stringify('production') }
-            })
+            }),
+
+            new webpack.optimize.UglifyJsPlugin()
         ]
     });
 }
